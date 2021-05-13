607ad21a80e2f91cbdf1b4de12c14db0e3c29a19
Irstenn <irstennbam@gmail.com>
2021-05-14 02:34:45 +0300

    Initial commit

---
+++ StudentsManager/Screens/DashboardForm.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace StudentsManager.Screens
+{
+    partial class DashboardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DashboardForm));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.NewStudenttoolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.ManagerBranchestoolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.EditProfiltoolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.ManagerUserstoolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.ManageReportstoolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
+            this.SystemSetuptoolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator6 = new System.Windows.Forms.ToolStripSeparator();
+            this.HelpandSupporttoolStripButton7 = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutStudentManagerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 72);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(653, 166);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NewStudenttoolStripButton,
+            this.toolStripSeparator1,
+            this.ManagerBranchestoolStripButton,
+            this.toolStripSeparator2,
+            this.EditProfiltoolStripButton,
+            this.toolStripSeparator3,
+            this.ManagerUserstoolStripButton,
+            this.toolStripSeparator4,
+            this.ManageReportstoolStripButton,
+            this.toolStripSeparator5,
+            this.SystemSetuptoolStripButton,
+            this.toolStripSeparator6,
+            this.HelpandSupporttoolStripButton7});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(856, 38);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // NewStudenttoolStripButton
+            // 
+            this.NewStudenttoolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("NewStudenttoolStripButton.Image")));
+            this.NewStudenttoolStripButton.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.NewStudenttoolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.NewStudenttoolStripButton.Name = "NewStudenttoolStripButton";
+            this.NewStudenttoolStripButton.Size = new System.Drawing.Size(79, 35);
+            this.NewStudenttoolStripButton.Text = "New Student";
+            this.NewStudenttoolStripButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 38);
+            // 
+            // ManagerBranchestoolStripButton
+            // 
+            this.ManagerBranchestoolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("ManagerBranchestoolStripButton.Image")));
+            this.ManagerBranchestoolStripButton.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.ManagerBranchestoolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ManagerBranchestoolStripButton.Name = "ManagerBranchestoolStripButton";
+            this.ManagerBranchestoolStripButton.Size = new System.Drawing.Size(109, 35);
+            this.ManagerBranchestoolStripButton.Text = "Manager Branches";
+            this.ManagerBranchestoolStripButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 38);
+            // 
+            // EditProfiltoolStripButton
+            // 
+            this.EditProfiltoolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("EditProfiltoolStripButton.Image")));
+            this.EditProfiltoolStripButton.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.EditProfiltoolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.EditProfiltoolStripButton.Name = "EditProfiltoolStripButton";
+            this.EditProfiltoolStripButton.Size = new System.Drawing.Size(62, 35);
+            this.EditProfiltoolStripButton.Text = "Edit Profil";
+            this.EditProfiltoolStripButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 38);
+            // 
+            // ManagerUserstoolStripButton
+            // 
+            this.ManagerUserstoolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("ManagerUserstoolStripButton.Image")));
+            this.ManagerUserstoolStripButton.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.ManagerUserstoolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ManagerUserstoolStripButton.Name = "ManagerUserstoolStripButton";
+            this.ManagerUserstoolStripButton.Size = new System.Drawing.Size(89, 35);
+            this.ManagerUserstoolStripButton.Text = "Manager Users";
+            this.ManagerUserstoolStripButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 38);
+            // 
+            // ManageReportstoolStripButton
+            // 
+            this.ManageReportstoolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("ManageReportstoolStripButton.Image")));
+            this.ManageReportstoolStripButton.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.ManageReportstoolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ManageReportstoolStripButton.Name = "ManageReportstoolStripButton";
+            this.ManageReportstoolStripButton.Size = new System.Drawing.Size(97, 35);
+            this.ManageReportstoolStripButton.Text = "Manage Reports";
+            this.ManageReportstoolStripButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.ManageReportstoolStripButton.Click += new System.EventHandler(this.toolStripButton5_Click);
+            // 
+            // toolStripSeparator5
+            // 
+            this.toolStripSeparator5.Name = "toolStripSeparator5";
+            this.toolStripSeparator5.Size = new System.Drawing.Size(6, 38);
+            // 
+            // SystemSetuptoolStripButton
+            // 
+            this.SystemSetuptoolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("SystemSetuptoolStripButton.Image")));
+            this.SystemSetuptoolStripButton.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.SystemSetuptoolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.SystemSetuptoolStripButton.Name = "SystemSetuptoolStripButton";
+            this.SystemSetuptoolStripButton.Size = new System.Drawing.Size(82, 35);
+            this.SystemSetuptoolStripButton.Text = "System Setup";
+            this.SystemSetuptoolStripButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripSeparator6
+            // 
+            this.toolStripSeparator6.Name = "toolStripSeparator6";
+            this.toolStripSeparator6.Size = new System.Drawing.Size(6, 38);
+            // 
+            // HelpandSupporttoolStripButton7
+            // 
+            this.HelpandSupporttoolStripButton7.Image = ((System.Drawing.Image)(resources.GetObject("HelpandSupporttoolStripButton7.Image")));
+            this.HelpandSupporttoolStripButton7.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.HelpandSupporttoolStripButton7.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.HelpandSupporttoolStripButton7.Name = "HelpandSupporttoolStripButton7";
+            this.HelpandSupporttoolStripButton7.Size = new System.Drawing.Size(104, 35);
+            this.HelpandSupporttoolStripButton7.Text = "Help and Support";
+            this.HelpandSupporttoolStripButton7.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.HelpandSupporttoolStripButton7.Click += new System.EventHandler(this.HelpandSupporttoolStripButton7_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(856, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutStudentManagerToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutStudentManagerToolStripMenuItem
+            // 
+            this.aboutStudentManagerToolStripMenuItem.Name = "aboutStudentManagerToolStripMenuItem";
+            this.aboutStudentManagerToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.aboutStudentManagerToolStripMenuItem.Text = "About Student Manager";
+            this.aboutStudentManagerToolStripMenuItem.Click += new System.EventHandler(this.aboutStudentManagerToolStripMenuItem_Click);
+            // 
+            // DashboardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 250);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "DashboardForm";
+            this.ShowInTaskbar = true;
+            this.Text = "DashboardForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.DashboardForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutStudentManagerToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton NewStudenttoolStripButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton ManagerBranchestoolStripButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton EditProfiltoolStripButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripButton ManagerUserstoolStripButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripButton ManageReportstoolStripButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
+        private System.Windows.Forms.ToolStripButton SystemSetuptoolStripButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator6;
+        private System.Windows.Forms.ToolStripButton HelpandSupporttoolStripButton7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ StudentsManager/Screens/DashboardForm.cs
@@ -0,0 +1,46 @@
+﻿using StudentsManager.Screens.Templates;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentsManager.Screens
+{
+    public partial class DashboardForm : TemplatesForm
+    {
+        public DashboardForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DashboardForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutStudentManagerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HelpandSupporttoolStripButton7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
